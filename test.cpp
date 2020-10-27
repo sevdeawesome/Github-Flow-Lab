447231848ef0b305721d914bacc98997e81059ec
DexterW1 <dexteryiumingwong@gmail.com>
2020-10-27 13:13:23 -0700

    added partner 2 changes

--- test.cpp
+++ test.cpp
@@ -1,45 +1,34 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
-TEST(EchoTest, HelloWorld){
-	char* test_val[3];
-	test_val[0] = "./c-echo";
-	test_val[1]="hello";
-	test_val[2]="world";
-	EXPECT_EQ("hello world", echo(3,test_val));
+TEST(EchoTest, HelloWorld) {
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
 }
-TEST(EchoTest,EmptyString){
-	char* test_val[1];
-	test_val[0]="./c-echo";
-	EXPECT_EQ("",echo(1,test_val));
+
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
-TEST(EchoTest,Numbers){
-	char* test_val[2];
-	test_val[0]="./c-echo";
-	test_val[1]="0123456789";
-	EXPECT_EQ("0123456789",echo(2,test_val));
+
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
-TEST(EchoTest,SpecialCharacters){
-	char* test_val[2];
-	test_val[0]="./c-echo";
-	test_val[1]="!@#$%^&*()<>?";
-	EXPECT_EQ("!@#$%^&*()<>?",echo(2,test_val));
+
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
-TEST(EchoTest,LongString){
-	char* test_val[9];
-	test_val[0]="./c-echo";
-	test_val[1]="Hi";
-	test_val[2]="my";
-	test_val[3]="name";
-	test_val[4]="is";
-	test_val[5]="Dexter";
-	test_val[6]="I";
-	test_val[7]="like";
-	test_val[8]="food";
-	EXPECT_EQ("Hi my name is Dexter I like food",echo(9,test_val));
+
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
-int main(int argc, char** argv){
-	::testing::InitGoogleTest(&argc,argv);
-	return RUN_ALL_TESTS();
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
